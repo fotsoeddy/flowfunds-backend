e8920d1c10bce32ec22a8c24886ea11ae1c6ebf7
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2026-01-16 17:14:37 +0100

    Register User, Account, and Transaction models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import User, Account, Transaction
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'first_name', 'last_name', 'is_staff', 'is_active', 'created_at')
+    search_fields = ('phone_number', 'first_name', 'last_name')
+    list_filter = ('is_staff', 'is_active')
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number', 'type', 'balance', 'currency', 'user', 'created_at')
+    search_fields = ('name', 'number', 'user__phone_number')
+    list_filter = ('type', 'currency')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('type', 'amount', 'category', 'reason', 'date', 'account', 'user')
+    search_fields = ('reason', 'category', 'user__phone_number', 'account__name')
+    list_filter = ('type', 'date')
